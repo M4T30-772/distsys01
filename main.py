9de8933161d26ad2bcac5f8d8c969b915c88a463
M4T30-772 <mateopakter772@gmail.com>
2023-06-16 18:07:27 +0200

    radi
    ovo radi

--- main.py
+++ main.py
@@ -1,13 +1,44 @@
 import json
+import asyncio
+import aiofiles
+import aiohttp
+import aiosqlite
+from aiohttp import web
 
-# Specify the file path
-file_path = 'working_json.json'
+routes = web.RouteTableDef()
 
-# Read the JSON file
-with open(file_path, 'r') as file:
-    data = json.load(file)
+async def json_data(request):
+    async with aiofiles.open('main.json', mode='r') as file:
+        print("JSON IS OPEN.")
+        read = [json.loads(line) async for line in file]
+        data = [
+            {
+                'username': item['repo_name'].rsplit('/', 1)[0],
+                'ghlink': f"https://github.com/{item['repo_name']}",
+                'filename': item['path'].rsplit('/', 1)[-1]
+            }
+            for item in read
+        ]
+        async with aiosqlite.connect('mydb.db') as db:
+            print("Connected to the database.")
+            await db.execute('CREATE TABLE IF NOT EXISTS mydb2 ('
+                             'username TEXT, ghlink TEXT, filename TEXT)')
+            print("Table 'mydb2' created.")
+            await db.executemany('INSERT INTO mydb2 (username, ghlink, filename) '
+                                 'VALUES (:username, :ghlink, :filename)', data)
+            print("Data inserted into the table.")
+            async with db.execute('SELECT * FROM mydb2 LIMIT 100') as cur:
+                columns = [column[0] for column in cur.description]
+                rows = await cur.fetchall()
+                result = [dict(zip(columns, row)) for row in rows]
+                print("Data fetched from the table:", result)
 
-# Print the contents of the JSON file
-for name in data:
-    if name.URL('d') or name.startswith('w'):
-        print(name)
+        async with aiohttp.ClientSession() as session:
+            async with session.post('http://127.0.0.1:8080', json=result) as resp:
+                assert resp.status == 200
+
+        return web.json_response(result)
+
+app = web.Application()
+app.router.add_routes([web.get('/Json', json_data)])
+web.run_app(app, port=8080)
